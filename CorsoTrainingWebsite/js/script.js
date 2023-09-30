c767a3077bbb0b6d9588560a2583f068ee409168
andrijat98 <andrijat98@gmail.com>
2023-09-30 18:24:07 +0200

    Added register area and nav scroll

--- CorsoTrainingWebsite/js/script.js
+++ CorsoTrainingWebsite/js/script.js
@@ -1,1 +1,13 @@
-// Add custom JavaScript here
+function userScrool() {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add('bg-dark');
+        } else {
+            navbar.classList.remove('bg-dark');
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', userScrool);
