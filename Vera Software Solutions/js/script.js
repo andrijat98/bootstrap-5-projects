5612687132a222f4c5c267c1a82e1b5315a526a2
andrijat98 <andrijat98@gmail.com>
2023-11-28 11:25:33 +0100

    crated navbar and hero section

---
+++ Vera Software Solutions/js/script.js
@@ -0,0 +1,12 @@
+// Replacing text in header
+const checkReplace = document.querySelector('.replace-me');
+
+if (checkReplace) {
+    const replace = new ReplaceMe(checkReplace, {
+        animation: 'animated fadeIn',
+        speed: 2000,
+        separator: ',',
+        loopCount: 'infinite',
+        autoRun: true
+    })
+}
